c8588d5d2adf240e82b9260310f19c2907ee3746
Ninad Patil (REIGNZ) <ninadpatil100@gmail.com>
2019-12-21 18:04:00 +0000

    rolex: Enable USB debugging at boot

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -188,3 +188,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
+
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
